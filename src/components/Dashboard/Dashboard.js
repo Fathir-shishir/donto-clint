61cd529af313bfece80fddb42767ff89f85c2c2b
Fathir Ahmed Shishir <mfa.shishir@gmail.com>
2022-04-05 02:47:24 +0200

    2 chart added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { LineChart, Line, XAxis, YAxis, PieChart, Pie, BarChart, CartesianGrid, Tooltip, Legend, Bar } from 'recharts';
 
 const Dashboard = () => {
+    const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
+    const[chart,setChart]= useState([])
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res=>res.json())
+        .then(data=>setChart(data))
+    },[])
     return (
         <div>
-            <h1>hello from dashboard</h1>
+       <LineChart width={400} height={500} data={chart}>
+       <Line type="monotone" dataKey="sell" stroke="#8884d8" />
+       <XAxis dataKey="month" />
+       <YAxis />
+       </LineChart>
+       
+       <BarChart width={730} height={500} data={chart}>
+  <CartesianGrid strokeDasharray="3 3" />
+  <XAxis dataKey="month" />
+  <YAxis />
+  <Tooltip />
+  <Legend />
+  <Bar dataKey="investment" fill="#8884d8" />
+  <Bar dataKey="revenue" fill="#82ca9d" />
+</BarChart>
         </div>
     );
 };
